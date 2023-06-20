0cab2fe81cf884f3617a6de5bdc9ded1461d58b7
AlejandroRR94 <rivasredondo94@gmail.com>
2023-06-20 13:13:37 +0200

    solving cache issues

--- src/models/utils.py
+++ src/models/utils.py
@@ -4,15 +4,14 @@ import os
 from pathlib import Path
 
 # Carga de datos de ejemplo
-@st.cache_data
-def load_data():
+# @st.cache_data
+def load_data(trial_data):
     """
     Carga por defecto los valores de data/raw/yahoo_data.xlsx
     """
     print('Directory Name:     ', os.path.dirname(__file__))
     directory = Path(os.path.dirname(__file__)).parent.absolute().parent.absolute()
     data_directory = os.path.join(directory, "data", "raw")
-    trial_data = st.selectbox("Archivos de prueba", [file for file in os.listdir(data_directory)])
     if trial_data is not None:
         df = pd.read_excel(os.path.join(directory, trial_data))  # Reemplazar por la ruta correcta del archivo
     else:
