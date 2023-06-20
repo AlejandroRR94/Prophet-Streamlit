3462cde7db4621fe6922d39199e83f6c4f6a4cae
AlejandroRR94 <rivasredondo94@gmail.com>
2023-06-20 12:45:49 +0200

    improved app distribution and added a utils.py file

--- src/models/prophet_app.py
+++ src/models/prophet_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 from prophet import Prophet
-
+from utils import *
 import os
 from pathlib import Path
 
@@ -11,24 +11,6 @@ st.set_page_config(page_title="Predicciones de Prophet", layout="wide")
 # Título de la aplicación
 st.title("Predicciones de Prophet")
 st.markdown("Aplicación de Streamlit para producir predicciones de Prophet sobre archivos que contienen series temporales")
-
-# DATACELL/prophet_app_st/data/raw/yahoo_data.xlsx
-
-
-# Carga de datos de ejemplo
-@st.cache_data
-def load_data():
-    print('Directory Name:     ', os.path.dirname(__file__))
-    directory = Path(os.path.dirname(__file__)).parent.absolute().parent.absolute()
-    df = pd.read_excel(os.path.join(directory, "data/raw/yahoo_data.xlsx"))  # Reemplazar por la ruta correcta del archivo
-    df.dropna(axis=0, inplace=True)
-
-
-    #Preparamos los datos en el formato reqerido por Prophet
-    # data = df[[time_variable, selected_y_var]]
-    # data = data.rename(columns={time_variable: 'ds', selected_y_var: 'y'})
-    return df
-
 
 data_file = st.file_uploader("Selecciona tu archivo excel para predecir la serie temporal \n(por defecto cargamos una serie temporal de yahoo)")
 if data_file is not None:
@@ -41,40 +23,40 @@ else:
     data = load_data()
 
 # MOSTRAMOS LOS DATOS
-st.write(data.head())
+st.write("Todos los datos")
+st.write(data.groupby("Country").head())
 st.write(data.shape)
 
+
+
+# Configuración de los parámetros ajustables
+st.sidebar.write("PARÁMETROS")
+st.sidebar.markdown("Modificar los siguientes parámetros para alterar el ajuste y la predicción del modelo")
+
+changepoint_prior_scale = st.sidebar.slider("changepoint_prior_scale", 0.0, 1.0, 0.05, 0.01)
+seasonality_prior_scale = st.sidebar.slider("seasonality_prior_scale", 0.0, 10.0, 1.0, 0.1)
+
+
 if "Country" in list(data.columns):
-    country = st.selectbox("Paises", data["Country"].unique())
+    country = st.sidebar.selectbox("Paises", data["Country"].unique())
+    data_country = data[data.Country == country]
+    # MOSTRAMOS LOS DATOS
+    st.write(f"Datos de {country}")
+    st.write(data_country.head())
+    st.write(data_country.shape)
+    data = data_country.copy()
 
-def select_variables(data:pd.DataFrame):
-    """
-    Seleccionamos dos variables mediante desplegables
-    """
-    selected_y_var = st.selectbox('**VARIABLE A PREDECIR?**',
-                                    list(data.columns)
-                                    )
-
-
-    time_variable = st.selectbox('**VARIABLE TEMPORAL?**',
-                                    list(data.drop([selected_y_var], 
-                                                    axis = 1).columns)
-                                                    )
-    return selected_y_var, time_variable
 
 # Selección de variables
 selected_y_var, time_variable = select_variables(data)
 data = data[[time_variable, selected_y_var]]
 data = data.rename(columns={time_variable: 'ds', selected_y_var: 'y'})
 
-# Filtrado del conjunto de datos
-# data_selected = data[selected_variables]
-# Configuración de los parámetros ajustables
-st.sidebar.write("PARÁMETROS")
-st.sidebar.markdown("Modificar los siguientes parámetros para alterar el ajuste y la predicción del modelo")
-changepoint_prior_scale = st.sidebar.slider("changepoint_prior_scale", 0.0, 1.0, 0.05, 0.01)
-seasonality_prior_scale = st.sidebar.slider("seasonality_prior_scale", 0.0, 10.0, 1.0, 0.1)
+
 days_to_forecast = st.sidebar.number_input(label='Días a predecir', min_value = 1, max_value=365, value = 30)
+
+
+
 
 
 
---
+++ src/models/utils.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import pandas as pd
+import os
+from pathlib import Path
+
+# Carga de datos de ejemplo
+@st.cache_data
+def load_data():
+    """
+    Carga por defecto los valores de data/raw/yahoo_data.xlsx
+    """
+    print('Directory Name:     ', os.path.dirname(__file__))
+    directory = Path(os.path.dirname(__file__)).parent.absolute().parent.absolute()
+    df = pd.read_excel(os.path.join(directory, "data/raw/yahoo_data.xlsx"))  # Reemplazar por la ruta correcta del archivo
+    df.dropna(axis=0, inplace=True)
+
+    return df
+
+def select_variables(data:pd.DataFrame):
+    """
+    Seleccionamos dos variables mediante desplegables
+    """
+    selected_y_var = st.selectbox('**VARIABLE A PREDECIR?**',
+                                    list(data.columns)
+                                    )
+
+
+    time_variable = st.selectbox('**VARIABLE TEMPORAL?**',
+                                    list(data.drop([selected_y_var], 
+                                                    axis = 1).columns)
+                                                    )
+    return selected_y_var, time_variable
